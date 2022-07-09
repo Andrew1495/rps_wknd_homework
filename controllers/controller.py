adce0af7c78b72c32957de4e643b82dddd7cb3d9
Andrew <andrewfairley95@gmail.com>
2022-07-09 12:37:31 +0100

    error 405 method not allowe dofr /computers

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,8 +1,8 @@
-from flask import render_template
+from flask import render_template , redirect, request
 from app import app
 from model.game import Game
 from model.player import Player
-
+import random
 
 
 @app.route('/game')
@@ -29,4 +29,27 @@ def rule():
 @app.route('/computer')
 def computer():
     return render_template("computer.html")
+
+@app.route('/computers',  methods=["GET", "POST"])
+def computers():
+    random_number = random.randint(1, 3)
+    if random_number == 1:
+        cp_choice = "rock"
+    elif random_number == 2:
+        cp_choice = "paper"
+    else:
+        cp_choice = "scissors"
+    player2 = Player("Computer", cp_choice)
+    player_name = request.form['name']
+    player_choice = request.form['choose']
+
+    player1 = Player(player_name, player_choice)
+
+    game = Game(player1, player2)
+    if winner == None:
+        win = f"Its a draw"
+    else:
+        win = f"The winner is:{winner.name}"
+    return render_template("computer.html", win=win)
+
 
